a601178bbcae32f7a17741bbc647f31e4cce5da6
Данил Хальпуков <danil@MacBook-Air-Danil.local>
2023-01-19 10:23:02 +0300

    I progress

--- src/js/files/script.js
+++ src/js/files/script.js
@@ -4,3 +4,21 @@ import { isMobile } from "./functions.js";
 import { flsModules } from "./modules.js";
 
 
+// Intro slide chande onhover 
+const introSliderCats = document.querySelectorAll('.categories-intro__item');
+
+function changeSlide(i) {
+
+}
+
+if (introSliderCats.length) {
+    introSliderCats.forEach((cat, i) => {
+        cat.addEventListener('mouseover', () => {
+            const introSliderDots = document.querySelectorAll('.intro__pagination .swiper-pagination-bullet');
+            introSliderDots[i].click();
+        });
+    })
+}
+
+//========================================================================================================================================================
+
