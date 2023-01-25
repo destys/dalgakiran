63f2593174d0d8e99461d5875f7707d48b37ecb7
Данил Хальпуков <danil@MacBook-Air-Danil.local>
2023-01-25 13:06:45 +0300

    In progress

--- src/js/files/sliders.js
+++ src/js/files/sliders.js
@@ -56,12 +56,90 @@ function initSliders() {
 			*/
 
 			// Пагинация
-			
+
 			pagination: {
 				el: '.intro__pagination',
 				clickable: true,
 			},
-			
+
+
+			// Скроллбар
+			/*
+			scrollbar: {
+				el: '.swiper-scrollbar',
+				draggable: true,
+			},
+			*/
+
+			// Кнопки "влево/вправо"
+			navigation: {
+				prevEl: '.swiper-button-prev',
+				nextEl: '.swiper-button-next',
+			},
+
+			// Брейкпоинты
+			/*
+			breakpoints: {
+				320: {
+					slidesPerView: 1,
+					spaceBetween: 0,
+					autoHeight: true,
+				},
+				768: {
+					slidesPerView: 2,
+					spaceBetween: 20,
+				},
+				992: {
+					slidesPerView: 3,
+					spaceBetween: 20,
+				},
+				1268: {
+					slidesPerView: 4,
+					spaceBetween: 30,
+				},
+			},
+			*/
+			// События
+			on: {
+
+			}
+		});
+	}
+	if (document.querySelector('.info-product__slider')) { // Указываем скласс нужного слайдера
+		// Создаем слайдер
+		new Swiper('.info-product__slider', { // Указываем скласс нужного слайдера
+			// Подключаем модули слайдера
+			// для конкретного случая
+			modules: [Navigation, Pagination],
+			observer: true,
+			observeParents: true,
+			slidesPerView: 1,
+			spaceBetween: 0,
+			autoHeight: false,
+			speed: 800,
+
+			//touchRatio: 0,
+			//simulateTouch: false,
+			//loop: true,
+			//preloadImages: false,
+			//lazy: true,
+
+			/*
+			// Эффекты
+			effect: 'fade',
+			autoplay: {
+				delay: 3000,
+				disableOnInteraction: false,
+			},
+			*/
+
+			// Пагинация
+
+			pagination: {
+				el: '.intro__pagination',
+				clickable: true,
+			},
+
 
 			// Скроллбар
 			/*
